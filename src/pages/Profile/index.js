0f7c1245f2b5b56228b8b29f07f08c233b095e04
Filipe Zaidan <41112779+filipezaidan@users.noreply.github.com>
2021-04-07 08:00:38 -0300

    changes styles in Profile

--- src/pages/Profile/index.js
+++ src/pages/Profile/index.js
@@ -1,10 +1,24 @@
-import React from 'react';
-import { View, Text} from 'react-native';
+import React, { useContext } from 'react';
+import { Container, Name, NewLink, NewText, Logout, LogoutText} from './styles';
+import { AuthContext } from '../../contexts/auth';
 
 export default function Profile() {
+
+  const { user, signOut} = useContext(AuthContext);
+
  return (
-   <View>
-       <Text>Profile</Text>
-   </View>
+   <Container>
+     <Name>{user && user.name}</Name>
+
+     <NewLink>
+      <NewText>Registrar</NewText>
+     </NewLink>
+
+    <Logout onPress={ () => signOut()}>
+      <LogoutText>Sair</LogoutText>
+    </Logout>
+
+   </Container>
+
   );
 }
